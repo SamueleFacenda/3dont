7497ddab79eb603595829d7ce49469fd3ede3cb1
SamueleFacenda <samuele.facenda@gmail.com>
2025-12-23 15:25:06 +0100

    Highlight entities when exploring trees in more cases
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- threedont/gui/points_tree_view/graph_tree_model.cpp
+++ threedont/gui/points_tree_view/graph_tree_model.cpp
@@ -151,7 +151,9 @@ void GraphTreeModel::onRowExpanded(const QModelIndex &index) {
   if (!index.isValid()) return;
   GraphTreeItem *item = itemFromIndex(index);
 
-  if (item->data(0) == "Constitutes")
+  if (item->data(0) == "Constitutes" ||
+      item->data(0) == "Is_part_of" ||
+      item->data(0) == "Has_as_part")
     controllerWrapper->selectAllSubjects(item->data(0, false).toString().toStdString(),
                                          item->data(1, false).toString().toStdString());
 
